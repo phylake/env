68f9b09d94be2dbd0b698d0988a4160fddf63498
Brandon Cook <phylake@gmail.com>
2013-05-12 19:43:45 -0700

    lucid haskell

---
+++ manifests/lucid_haskell.sh
@@ -0,0 +1,21 @@
+sudo apt-get update
+
+yes | sudo apt-get install build-essential
+yes | sudo apt-get install libqt4-opengl-dev
+yes | sudo apt-get install libglut3-dev
+yes | sudo apt-get install libgmp3-dev
+yes | sudo apt-get install curl
+
+curl http://www.haskell.org/ghc/dist/7.4.2/ghc-7.4.2-i386-unknown-linux.tar.bz2 -o ghc-7.4.2-i386-unknown-linux.tar.bz2
+curl http://lambda.haskell.org/platform/download/2012.4.0.0/haskell-platform-2012.4.0.0.tar.gz -o haskell-platform-2012.4.0.0.tar.gz
+tar -xzf haskell-platform-2012.4.0.0.tar.gz
+tar -xjf ghc-7.4.2-i386-unknown-linux.tar.bz2
+
+cd ghc-7.4.2
+sudo ./configure && sudo make install
+
+cd ../haskell-platform-2012.4.0.0
+sudo ./configure && sudo make install
+
+cabal update
+cabal install cabal-install
