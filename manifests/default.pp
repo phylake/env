7938f987ada2728b310b6eee394d4a49a4304d4e
Brandon Cook <phylake@gmail.com>
2013-05-19 16:03:12 -0700

    turned packages on for now

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,12 +1,12 @@
 Exec { path => ['/usr/bin'] }
 
-@exec {'sudo apt-get update':
-  tag => update
-}
-Exec <| tag == update |> -> Package <| |>
+# @exec {'sudo apt-get update':
+#   tag => update
+# }
+# Exec <| tag == update |> -> Package <| |>
 
-# exec {'sudo apt-get update':}
-# Exec['sudo apt-get update'] -> Package <| |>
+exec {'sudo apt-get update':}
+Exec['sudo apt-get update'] -> Package <| |>
 
 # @exec {'curl':
 #   tag => echo
@@ -14,21 +14,21 @@ Exec <| tag == update |> -> Package <| |>
 # Package <| |> -> Exec <| tag == echo |>
 
 class packages {
-  # package { 'git':
-  #   ensure => installed
-  # }
-  # package { 'unzip':
-  #   ensure => installed
-  # }
+  package { 'git':
+    ensure => installed
+  }
+  package { 'unzip':
+    ensure => installed
+  }
   package { 'curl':
     ensure => installed
   }
-  # package { 'llvm':
-  #   ensure => installed
-  # }
-  # package { 'clang':
-  #   ensure => installed
-  # }
+  package { 'llvm':
+    ensure => installed
+  }
+  package { 'clang':
+    ensure => installed
+  }
 }
 
 include packages
