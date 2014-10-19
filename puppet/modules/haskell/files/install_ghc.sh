e6866044908699758c6ba2c8a8dacb0766149066
Brandon Cook <phylake@gmail.com>
2014-10-19 09:45:47 -0700

    dependencies; git://

--- puppet/modules/haskell/files/install_ghc.sh
+++ puppet/modules/haskell/files/install_ghc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 cabal update &&
-cabal install cabal-install &&
+cabal install cabal-install
+
+sudo apt-get -y install haskell-platform git autoconf automake libtool make libgmp-dev ncurses-dev g++ llvm
+
 cabal install -j16 alex happy &&
-# git clone git@github.com:ghc/ghc.git &&
+# git clone git://github.com/ghc/ghc.git &&
 git clone git://git.haskell.org/ghc.git &&
 cd ghc &&
 ./sync-all --no-dph get &&
